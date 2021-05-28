cecefc6ceeb00d136810f388956c82e5d9ae359b
hidehish <hidehish+git@gmail.com>
2021-05-28 21:22:50 +0900

    initial commit

---
+++ client/src/locales/ja/embed.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail またはユーザー名',
+      invalidEmailOrUsername: 'E-mail またはユーザー名が違います。',
+      invalidPassword: 'パスワードが違います。',
+      logInToPlanka: 'ログイン',
+      noInternetConnection: 'ネットに接続されていません。',
+      pageNotFound_title: 'ページがありません。',
+      password: 'パスワード',
+      projectManagement: 'プロジェクト管理',
+      serverConnectionFailed: 'サーバー接続失敗',
+      unknownError: '不明なエラー。再度お試しください。',
+    },
+
+    action: {
+      logIn: 'ログイン',
+    },
+  },
+};
